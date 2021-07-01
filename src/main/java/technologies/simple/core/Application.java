5f8d23781bdb77a8dd491731c903274f20115614
Ranjan Srivastava <ranjanks@gmail.com>
2021-07-01 19:18:47 +0530

    added junit

--- src/main/java/technologies/simple/core/Application.java
+++ src/main/java/technologies/simple/core/Application.java
@@ -1,14 +1,20 @@
 package technologies.simple.core;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words,' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
+	
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
-		greetings.add("Hello");
+		greetings.add("Hello ");
 		for (String greeting: greetings) {
-			System.out.println("Greetings"+greeting);
+			System.out.println("Greetings "+greeting);
 		}	
 	}
 	
@@ -19,7 +25,9 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
-	app.greet();
+		Application app = new Application();
+		app.greet();
+		//int count = app.countWords("I have four words");
+		//System.out.println("Words count "+count);
     }
 }
